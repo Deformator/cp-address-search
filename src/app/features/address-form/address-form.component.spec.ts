dcae40c2d33fcece9e6fae609bd71c41b8a83b0c
Deformator <dammandrii@gmail.com>
2021-01-24 16:59:00 -0700

    feat: styling

--- src/app/features/address-form/address-form.component.spec.ts
+++ src/app/features/address-form/address-form.component.spec.ts
@@ -1,10 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
 import { Store } from '@ngrx/store';
+import { AppCommonModule } from 'src/app/common/common.module';
 import { CoreModule } from 'src/app/common/core/core.module';
 import { StoreService } from 'src/app/common/core/services/store.service';
 import { SharedModule } from 'src/app/common/shared/shared.module';
 import { CanadaPostStoreService } from '../services/canadaPost-store.service';
+import { CanadaPostService } from '../services/canadaPost.service';
 
 import { AddressFormComponent } from './address-form.component';
 
@@ -15,8 +17,8 @@ describe('AddressFormComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [AddressFormComponent],
-      providers: [CanadaPostStoreService],
-      imports: [ReactiveFormsModule, SharedModule]
+      providers: [CanadaPostStoreService, CanadaPostService],
+      imports: [ReactiveFormsModule, AppCommonModule]
     }).compileComponents();
   });
 
